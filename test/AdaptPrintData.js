5bf8b5f867b47d3382a31ef36ed44c30b82fc2d8
Lasse <lasse@haslev.no>
2017-10-18 19:24:24 +0200

    Prepared test to write

---
+++ test/AdaptPrintData.js
@@ -0,0 +1,7 @@
+import test from 'ava';
+
+// It can access adapt_data through API
+// It can get; and prefer getting local data over API connection
+// It cleans up DOM after template has run
+// it takes the template element and adds it to element, then makes it available to class through this.template
+// It runs script after template is rendered
