6dd551d82c06fe480c88fa2f55909c8297a71db1
Lasse <lasse@haslev.no>
2017-10-18 22:32:32 +0200

    Prepared for running adapt tests

--- test/AdaptPrintData.js
+++ test/AdaptPrintData.js
@@ -1,8 +1,30 @@
 import test from 'ava';
 import AdaptDataGetDataFromAPI from './fixtures/AdaptDataGetDataFromAPI';
 
-const createTestingEnvironment = () => {
-    console.log(window);
+const resetTestingEnvironment = () => {
+    document.body.removeAttribute( 'id' );
+    document.body.innerHTML = '';
+}
+const setupTestingEnvironment = () => {
+    // Set id of body element
+    document.body.setAttribute( 'id', 'adapt' );
+
+    document.body.insertAdjacentHTML( 'beforeEnd', `
+        <div class="data_box template_id_3" data-productionpagedata_id="24" data-template_id="3">
+            <div class="attributes">
+                <div class="attribute a_id" data-attribute_type="textSingle" data-attribute_value="">XD-1428</div>
+                <div class="attribute a_name" data-attribute_type="textSingle" data-attribute_value="">Sykkelveske</div>
+                <div class="attribute a_link" data-attribute_type="textSingle" data-attribute_value="">http://www.gosh.no/no_no/product/sykkelveske-1</div>
+                <div class="attribute a_description" data-attribute_type="textMulti" data-attribute_value="">Sykkelveske til å feste på rammen.&nbsp; 600D polyester.&nbsp; Str. 27 x 16 x 5 cm.&nbsp; 1 stort rom med glidelås.&nbsp; 4 farger.</div>
+                <div class="attribute a_image" data-attribute_type="image" data-attribute_value="">
+                    <div class="image_wrapper">
+                        <img src="http://image.promoworld.ca/migration-api-hidden-new/web/images/1066/xd-1428-blue.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    ` );
+
 };
 
 test( 'It can access adapt_data through API and format its response', async t => {
@@ -16,6 +38,16 @@ test( 'It can access adapt_data through API and format its response', async t =>
 } );
 
 // It can set the adaptData through constructor method
+
+test( 'It can get and prefer getting local data over API connection', t => {
+    setupTestingEnvironment();
+
+    t.true( true );
+    // console.log(document.documentElement.innerHTML);
+
+    resetTestingEnvironment();
+} );
+
 // It cleans up DOM after template has run
 // it takes the template element and adds it to element, then makes it available to class through this.template
 // It runs script after template is rendered
