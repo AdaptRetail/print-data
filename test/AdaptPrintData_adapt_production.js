d1eaba0548471dd671e0892aaee396e6efefa8f7
Lasse <lasse@haslev.no>
2017-10-21 11:59:38 +0200

    Made test work with multiple .data_box es

--- test/AdaptPrintData_adapt_production.js
+++ test/AdaptPrintData_adapt_production.js
@@ -10,10 +10,19 @@ test.beforeEach( t => {
     document.body.insertAdjacentHTML( 'beforeEnd', `
         <div class="data_box template_id_3" data-productionpagedata_id="24" data-template_id="3">
             <div class="attributes">
+                <div class="attribute a_id" data-attribute_type="textSingle" data-attribute_value="">XD-1429</div>
+                <div class="attribute a_name" data-attribute_type="textSingle" data-attribute_value="">Sykkelbukse</div>
+                <div class="attribute a_image" data-attribute_type="image" data-attribute_value="">
+                    <div class="image_wrapper">
+                        <img src="http://image.promoworld.ca/migration-api-hidden-new/web/images/1066/xd-1429-sykkelbukse.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="data_box template_id_3" data-productionpagedata_id="24" data-template_id="3">
+            <div class="attributes">
                 <div class="attribute a_id" data-attribute_type="textSingle" data-attribute_value="">XD-1428</div>
                 <div class="attribute a_name" data-attribute_type="textSingle" data-attribute_value="">Sykkelveske</div>
-                <div class="attribute a_link" data-attribute_type="textSingle" data-attribute_value="">http://www.gosh.no/no_no/product/sykkelveske-1</div>
-                <div class="attribute a_description" data-attribute_type="textMulti" data-attribute_value="">Sykkelveske til å feste på rammen.&nbsp; 600D polyester.&nbsp; Str. 27 x 16 x 5 cm.&nbsp; 1 stort rom med glidelås.&nbsp; 4 farger.</div>
                 <div class="attribute a_image" data-attribute_type="image" data-attribute_value="">
                     <div class="image_wrapper">
                         <img src="http://image.promoworld.ca/migration-api-hidden-new/web/images/1066/xd-1428-blue.jpg">
@@ -46,7 +55,7 @@ test( 'It can get image attribute from DOM element', async t => {
 test( 'It takes the template function and adds it to element, then makes it available to class through this.template', t => {
     let adaptData = new AdaptDataGetDataFromAPI;
 
-    t.is( adaptData.template.outerHTML, document.querySelector( '.data_box' ).outerHTML );
+    t.is( adaptData.template.outerHTML, document.querySelectorAll( '.data_box' )[1].outerHTML );
 } );
 
 test( 'It puts template content to element removes attributes element on DOM after template has run', async t => {
@@ -67,9 +76,14 @@ test( 'It runs script function after template is rendered', async t => {
     let adaptData = new AdaptData_script;
     await adaptData.onReady;
 
-    var heading = document.querySelector( 'h1' );
+    var heading = document.querySelectorAll( 'h1' )[1];
     t.is( 'Taken over', heading.innerHTML );
 
 } );
-// It runs script function after template is rendered
-// It changes all DOME elements with this class logic
+
+// test( 'It changes all DOME elements with this class logic', t => {
+    // let adaptData = new AdaptDataGetDataFromAPI;
+    // await adaptData.onReady;
+
+
+// } );
